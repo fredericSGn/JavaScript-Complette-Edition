b626ae1bc48f2e40d23511b135892b48483bab27
Frederic <fc.nowacki@gmail.com>
2024-01-10 16:30:15 +0100

    1.3

---
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,15 @@
+//Values and Variables
+/*
+let country = "Poland";
+let continent = "Europe";
+let population = 37.75 + "mill";
+
+let isIsland = false;
+const language = "polisch";
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
+*/
