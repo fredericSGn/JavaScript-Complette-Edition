bc474c5ec30048a0b889441759e212df5388b651
Frederic <fc.nowacki@gmail.com>
2024-02-26 15:10:41 +0100

    1.11

--- complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -163,7 +163,6 @@ console.log("23" / "2");
 let n = "1" + 1; // '11'
 n = n - 1;
 console.log(n);
-*/
 
 // 5 falsy values: 0, '', undefined, null, NaN
 console.log(Boolean(0));
@@ -185,3 +184,26 @@ if (height) {
 } else {
   console.log("Height is UNDEFINED!");
 }
+*/
+
+//Equality Operators: == vs. ===
+const age = "18";
+if (age === 18) console.log("You just became an adult :D (strict)");
+
+if (age == 18) console.log("You just became an adult :D (loose)");
+
+const favourite = Number(prompt("What's your favorite number?"));
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  //22 === 23 -> False
+  console.log("Cool! 23 ist an amazing number!");
+} else if (favourite === 7) {
+  console.log("7 is also a cool number");
+} else if (favourite === 9) {
+  console.log("9 is also a cool number");
+} else {
+  console.log("Number is not 23 or 7");
+}
+
+if (favourite !== 23) console.log("Why not 23?");
