57678ec76f9bab557737ca1670ce29a34c64e4cb
Frederic <fc.nowacki@gmail.com>
2024-02-26 14:20:18 +0100

    1.10

--- complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -142,7 +142,7 @@ if (birthYear <= 2000) {
 }
 
 console.log(century);
-*/
+
 
 // type conversion
 const inputYear = "1991";
@@ -163,3 +163,25 @@ console.log("23" / "2");
 let n = "1" + 1; // '11'
 n = n - 1;
 console.log(n);
+*/
+
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Frederic"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 10;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+let height = 0;
+if (height) {
+  console.log("YAY! Height ist defined");
+} else {
+  console.log("Height is UNDEFINED!");
+}
